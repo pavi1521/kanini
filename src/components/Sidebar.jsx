f032c560aff9816b8255c7d35aa2dc1254fe2661
pavithra <pavithiru04@gmail.com>
2024-07-09 11:12:58 +0530

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img className="image" src="/download.png" alt="kanini" />
+      <span className="h1">Kanini</span>
+      <br />
+      <span className="px-5 ms-5">Assessment</span>
+
+      <ul className="list-group list-group-flush">
+        <li className="nav-item my-2 px-3 py-2">
+          <i className="bi bi-intersect fs-5 me-2"></i>
+          <span>Dashboard</span>
+        </li>
+        <li className="nav-item  list-group-item my-2 py-2 px-2">
+          <i className="bi bi-journal fs-4 me-2"></i>
+          <span>Questions</span>
+        </li>
+        <li className="nav-item list-group-item  px-2 my-2 py-2">
+          <i className="bi bi-bag fs-4 me-2"></i>
+          <span>Jobseeker</span>
+        </li>
+        <li className="nav-item list-group-item px-2 my-2 py-3">
+          <i className="bi bi-person-vcard-fill me-3"></i>
+          <span>Employee</span>
+        </li>
+        <li className="nav-item list-group-item px-2  my-2">
+          <i className="bi bi-gear fs-4 me-2"></i>
+          <span>Settings</span>
+        </li>
+        
+          <li className="list-group-item logout" src="/Mask.png" alt="kanini
+          ">
+             </li>
+            <img className='image1
+             src="/Mask.png" alt="kanini" />
+            <li className='nav-item button py-3 px-3 my-4 list-group-item'>
+            <i class="bi bi-box-arrow-right"></i>
+              <span className="log">Logout</span>
+            </li>
+
+        
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
